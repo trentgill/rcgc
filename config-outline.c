ee87ac65926132bfd92c83d0d17f48e6421b2d56
Trent <trent.gill@gmail.com>
2017-05-30 15:32:41 -0400

    updated config.json file for chooser

--- config-outline.c
+++ config-outline.c
@@ -8,6 +8,10 @@
 - paths of splash / gameplay image(s) // for the launcher
 
 
+// input:
+	~/path/to/game/config
+// output
+
 
 
 // the below could be automated with a web app
@@ -16,24 +20,22 @@
 
 {
 	// for the launcher
-	path       = /main.game;
-	engine     = Python;
-	keyboard   = (not supported / optional / required);
-	mouse      = (not supported / optional / required);
-	gamepad    = (not supported / optional / required);
-	network    = (not supported / optional / required);
-	multiplayer = (local / network / both / none);
-	singleplayer = (t / f);
-
+	"path": "/main.lua",
+	"engine": "Python",
+	"keyboard": "(not supported / optional / required)",
+	"mouse": "(not supported / optional / required)",
+	"gamepad": "(not supported / optional / required)",
+	"network": "(not supported / optional / required)",
+	"multiplayer": "(local / network / both / none)",
+	"singleplayer": "(t/f)",
 
 	// for the selector
-	author     = "Mr Big Shot";
-	batch      = "SP2'17";
-	website    = "http://github.com/mrbigshot";
-	splash     = "rcgc/splash.png";
-	screenshot = "rcgc/screenshot.png";
+	"author": "ur name",
+	"batch": "ur batch",
+	"website": "github maybe?",
+	"splash": "rcgc/splash.png",
+	"screenshot": "rcgc/screenshot.png"
 }
-
 
 /*
 on boot:
