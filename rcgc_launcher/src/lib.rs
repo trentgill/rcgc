7acdd56abe3f1697a34c939495b8e326a152b758
Trent <trent.gill@gmail.com>
2017-06-01 13:12:46 -0400

    refactor stderr messages. cleanup

--- rcgc_launcher/src/lib.rs
+++ rcgc_launcher/src/lib.rs
@@ -4,6 +4,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use json::JsonValue;
+use std::process;
 
 mod parse_json;
 
@@ -31,4 +32,17 @@ pub fn path_to_json(path: &String) -> Result<JsonValue, Box<Error>> {
     file.read_to_string(&mut contents)?;
     let json = parse_json::parse_json(&contents)?;
     Ok(json)
+}
+
+pub fn unwrap_or_stderr<T, E>(burrito: Result<T, E>, msg: &str) -> T
+        where E: std::fmt::Display {
+    burrito.unwrap_or_else( |err| {
+        writeln!(
+            std::io::stderr(),
+            "{}: {}",
+            msg,
+            err
+            ).expect("Unable to write to stderr");
+        process::exit(1);
+    })
 }
--- rcgc_launcher/src/main.rs
+++ rcgc_launcher/src/main.rs
@@ -2,53 +2,32 @@ extern crate rcgc_launcher;
 extern crate json;
 
 use std::env;
-//use std::fs::File;
-use std::io::prelude::*;
-use std::process;
 use std::process::Command;
 use rcgc_launcher::Config;
-//use json::JsonValue;
+use rcgc_launcher::unwrap_or_stderr;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut stderr = std::io::stderr();
-    let config = Config::new(&args).unwrap_or_else( |err| {
-        writeln!(
-            &mut stderr,
-            "Problem parsing arguments: {}",
-            err
-            ).expect("Unable to write to stderr");
-        process::exit(1);
-    });
-
+    let config = unwrap_or_stderr(
+        Config::new(&args),
+        "Problem parsing arguments"
+    );
 
     let json_path = format!("{}config.json", config.path);
-    let json = rcgc_launcher::path_to_json(&json_path)
-        .unwrap_or_else( |err| {
-            writeln!(
-                &mut stderr,
-                "Problem parsing json: {}",
-                err
-            ).expect("Unable to write to stderr");
-            process::exit(1);
-        });
+    let json = unwrap_or_stderr(
+        rcgc_launcher::path_to_json(&json_path),
+        "Problem parsing config.json"
+    );
     
     let engines_path = String::from("engines.json");
-    let engines = rcgc_launcher::path_to_json(&engines_path)
-        .unwrap_or_else( |err| {
-            writeln!(
-                &mut stderr,
-                "Problem parsing json: {}",
-                err
-            ).expect("Unable to write to stderr");
-            process::exit(1);
-        });
+    let engines = unwrap_or_stderr(
+        rcgc_launcher::path_to_json(&engines_path),
+        "Problem parsing engines.json"
+    );
+
     let engine = json["engine"].as_str().unwrap_or("");
-    println!("engine string: {}", &engine);
     let interpreter_cmd = engines[engine].as_str().unwrap_or("");
-    println!("engines[{}]: {}", &engine, &interpreter_cmd);
-    println!("engines[\"Love2D\"]: {}", engines["Love2D"].dump());
- 
+    
     println!(
         "what the shell sees: {} {}{}",
         interpreter_cmd,
