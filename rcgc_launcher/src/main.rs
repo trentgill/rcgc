c2f5f40fec3b76722038601a9827f27e7ffaad74
agevitam <mastersebastian@gmail.com>
2017-06-21 16:34:06 -0400

    add logic for launching web apps

--- rcgc_launcher/src/main.rs
+++ rcgc_launcher/src/main.rs
@@ -1,6 +1,8 @@
 extern crate rcgc_launcher;
 extern crate json;
 
+use std::fs::File;
+use std::io::prelude::*;
 use std::env;
 use std::process::Command;
 use rcgc_launcher::ShellArgs;
@@ -31,17 +33,46 @@ fn main() {
         .as_str().unwrap_or("");
     
     rcgc_launcher::check_devices(&config);
+    
     //eg: how to run a formatted shell command in a sub-terminal
     //println!("Argument: {}", shell_args.path);
-    let launch_cmd = format!(
-        "'{} {}'",
-        engine_cmd,
-        shell_args.path
-    );
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(launch_cmd)
-        .output()
-        .expect("Unable to execute shell command");
-    println!("output: {}", String::from_utf8_lossy(&output.stdout));
+    match selected_engine {
+        "Web" => {
+            // echo 'chromium-browser --app=http://example.com' >
+            // ~/.xinitrc && startx && rm ~/.xinitrc
+            let url = config["path"].as_str().unwrap_or("");
+            let web_cmd = format!("chromium-browser --app={}", url);
+            let web_cmd_bytes = web_cmd.into_bytes();
+            let mut xinitrc = unwrap_or_stderr(
+                File::create("~/.xinitrc"),
+                "Unable to create ~/.xinitrc"
+            );
+            unwrap_or_stderr(
+                xinitrc.write_all(&web_cmd_bytes),
+                "Unable to write to ~/.xinitrc"
+            );
+            xinitrc.sync_all().unwrap();
+
+            Command::new("startx")
+                .output()
+                .expect("Unable to startx");
+            Command::new("rm")
+                .arg("~/.xinitrc")
+                .output()
+                .expect("Unable to rm ~/.xinitrc");
+        }
+        _ => {
+            let launch_cmd = format!(
+                "'{} {}'",
+                engine_cmd,
+                shell_args.path
+            );
+            let output = Command::new("sh")
+                .arg("-c")
+                .arg(launch_cmd)
+                .output()
+                .expect("Unable to execute shell command");
+            println!("output: {}", String::from_utf8_lossy(&output.stdout));
+        }
+    }
 }
